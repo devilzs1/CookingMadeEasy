373bd3d528e06640b426e9c23524610ce7131b86
Mohammad Adil <mohammadadil3292@gmail.com>
2023-08-12 11:17:58 +0530

    Modify database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI1 + "recipeMadeEasy" + process.env.MONGODB_URI2);
-// mongoose.connect(process.env.MONGODB_URI);
 
-const db = mongoose.connection;
-db.on('error',console.error.bind(console,'connection error:'));
-db.once('open',()=>{
-    console.log('Connected')
-});
+const dbUrl = (process.env.NODE_ENV && process.env.NODE_ENV === 'production') ? process.env.MONGODB_URI1 + "recipeMadeEasy" + process.env.MONGODB_URI2 : process.env.MONGODB_URI;
+
+// mongoose.connect(dbUrl);
+// // mongoose.connect(process.env.MONGODB_URI);
+
+// const db = mongoose.connection;
+// db.on('error',console.error.bind(console,'connection error:'));
+// db.once('open',()=>{
+//     console.log('Connected')
+// });
+
+mongoose.set("strictQuery", true);
+
+try {
+  const conn = mongoose.connect(dbUrl);
+  console.log("Database Connected!");
+} catch (error) {
+  console.log(`Database Connection Error! : ${error}`);
+}
 
 require('./Category');
 require('./Recipe');
