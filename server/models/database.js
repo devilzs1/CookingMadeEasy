651517f2435d6dc328a3230ea16a86bd3dd522ae
Mohammad Adil <97207850+devilzs1@users.noreply.github.com>
2023-08-14 21:43:12 +0530

    commit

--- server/models/database.js
+++ server/models/database.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
 
-const dbUrl = (process.env.NODE_ENV && process.env.NODE_ENV === 'production') ? process.env.MONGODB_URI1 + "recipeMadeEasy" + process.env.MONGODB_URI2 : process.env.MONGODB_URI;
+const dbUrl = (process.env.NODE_ENV && process.env.NODE_ENV === 'production')
+  ? process.env.MONGODB_URI1 + "recipeMadeEasy" + process.env.MONGODB_URI2
+  : process.env.MONGODB_URI;
 
-// mongoose.connect(dbUrl);
-// // mongoose.connect(process.env.MONGODB_URI);
-
-// const db = mongoose.connection;
-// db.on('error',console.error.bind(console,'connection error:'));
-// db.once('open',()=>{
-//     console.log('Connected')
-// });
-console.log(dbUrl);
 mongoose.set("strictQuery", true);
 
-try {
-  const conn = mongoose.connect(dbUrl);
-  console.log("Database Connected!");
-} catch (error) {
-  console.log(`Database Connection Error! : ${error}`);
-}
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database Connected!");
+  })
+  .catch(error => {
+    console.error(`Database Connection Error: ${error}`);
+  });
 
 require('./Category');
 require('./Recipe');
